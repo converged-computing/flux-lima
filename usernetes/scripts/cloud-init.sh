b44fecbaef8b4c29387409e79184b6f831bd0ea8
vsoch <vsoch@users.noreply.github.com>
2023-12-16 17:45:44 -0700

    ensure we add hostnames to /etc/hosts
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/cloud-init.sh
+++ usernetes/scripts/cloud-init.sh
@@ -5,6 +5,13 @@ curl https://www-csp.llnl.gov/content/assets/csoc/cspca.crt > cspca.crt
 cp cspca.crt /usr/local/share/ca-certificates
 update-ca-certificates
 
+# Add other nodes to /etc/hosts, manual and not great, but will get the job done!
+# These are in ansible-hosts
+echo "192.168.65.121 u2204-01" >> /etc/hosts
+echo "192.168.65.122 u2204-02" >> /etc/hosts
+echo "192.168.65.123 u2204-03" >> /etc/hosts
+echo "192.168.65.124 u2204-04" >> /etc/hosts
+
 wget https://raw.githubusercontent.com/converged-computing/flux-lima/main/usernetes/scripts/install-flux.sh
 wget https://raw.githubusercontent.com/converged-computing/flux-lima/main/usernetes/scripts/install-usernetes.sh
 chmod +x ./install-flux.sh
