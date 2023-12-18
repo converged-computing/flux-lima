d51ad30306018a469d31fdf167fa1fe42f1a567d
vsoch <vsoch@users.noreply.github.com>
2023-12-18 13:27:32 -0700

    add resolv.conf entries
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/cloud-init.sh
+++ usernetes/scripts/cloud-init.sh
@@ -6,11 +6,16 @@ cp cspca.crt /usr/local/share/ca-certificates
 update-ca-certificates
 
 # Add other nodes to /etc/hosts, manual and not great, but will get the job done!
-# These are in ubuntu-vars.yaml
+# These are in ubuntu-vars.yaml. This was our first approach...
 echo "192.168.65.121 u2204-01" >> /etc/hosts
 echo "192.168.65.122 u2204-02" >> /etc/hosts
 echo "192.168.65.123 u2204-03" >> /etc/hosts
 echo "192.168.65.124 u2204-04" >> /etc/hosts
+
+# This is a more robust approach (that will be an improvement over the above)
+# we can likely remove the above but not tested yet.
+echo "search llnl.gov" >> /etc/resolv.conf
+echo "nameserver 192.168.65.1" >> /etc/resolv.conf
 
 wget https://raw.githubusercontent.com/converged-computing/flux-lima/main/usernetes/scripts/install-flux.sh
 wget https://raw.githubusercontent.com/converged-computing/flux-lima/main/usernetes/scripts/install-usernetes.sh
