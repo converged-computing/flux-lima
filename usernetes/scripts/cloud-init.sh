7069897fb46a53c7a4749127c4686ad07c6df11e
vsoch <vsoch@users.noreply.github.com>
2023-12-20 00:23:19 -0700

    typo in script names
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/cloud-init.sh
+++ usernetes/scripts/cloud-init.sh
@@ -18,9 +18,10 @@ echo "search llnl.gov" >> /etc/resolv.conf
 echo "nameserver 192.168.65.1" >> /etc/resolv.conf
 
 for script in flux usernetes lammps singularity; do
+    echo "Installing $script"
     wget https://raw.githubusercontent.com/converged-computing/flux-lima/main/usernetes/scripts/install-${script}.sh
-    chmod +x ./${script}.sh
-    /bin/bash ./${script}.sh
+    chmod +x ./install-${script}.sh
+    /bin/bash ./install-${script}.sh
 done
 
 # Start flux at the end!
