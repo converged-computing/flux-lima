adb43a9786fd453eb821856c55c92e03119bc1f0
vsoch <vsoch@users.noreply.github.com>
2023-12-23 11:51:01 -0700

    ensure worker of pytorch is consistent with 5 epochs
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/cloud-init.sh
+++ usernetes/scripts/cloud-init.sh
@@ -5,6 +5,16 @@ curl https://www-csp.llnl.gov/content/assets/csoc/cspca.crt > cspca.crt
 cp cspca.crt /usr/local/share/ca-certificates
 update-ca-certificates
 
+# Add other nodes to /etc/hosts, manual and not great, but will get the job done!
+# These are in ubuntu-vars.yaml. This was our first approach...
+echo "192.168.65.121 u2204-01" >> /etc/hosts
+echo "192.168.65.122 u2204-02" >> /etc/hosts
+echo "192.168.65.123 u2204-03" >> /etc/hosts
+echo "192.168.65.124 u2204-04" >> /etc/hosts
+echo "192.168.65.125 u2204-05" >> /etc/hosts
+echo "192.168.65.126 u2204-06" >> /etc/hosts
+echo "192.168.65.127 u2204-07" >> /etc/hosts
+
 # This is a more robust approach (that will be an improvement over the above)
 # we can likely remove the above but not tested yet.
 echo "search llnl.gov" >> /etc/resolv.conf
