43dce264233415ca23246642b97b2bf9824753a2
vsoch <vsoch@users.noreply.github.com>
2023-12-29 07:23:16 -0700

    update launch container for kubernetes
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/mnist/launch-container.sh
+++ usernetes/scripts/mnist/launch-container.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-job_name="flux-sample"
+
+# We are hardcoding these for now, would be better to use envars
+# Get the number of nodes (I think JOB_COMPLETIONS would work here)
 job_port="8080"
+nodes=6
+leader=flux-sample-0
 
-# Read all the hostname the job is running
-nodenames=$(flux exec -r all hostname)
-echo "Found nodes ${nodenames}"
-
-# Get the number of nodes
-nodes=$(echo "${#node_names[@]}")
-leader=$(flux exec -r 0 hostname)
 echo "The leader broker is ${leader}"
-nodes=${FLUX_JOB_NNODES}
-rank=${FLUX_TASK_RANK}
+rank=${JOB_COMPLETION_INDEX}
 echo "I am hostname $(hostname) and rank ${rank} of ${nodes} nodes"
 
-time torchrun --node_rank ${LOCAL_RANK} --nnodes ${nodes} --nproc_per_node 8 --master_addr ${leader} --master_port ${job_port} /main.py
+time torchrun --node_rank ${rank} --nnodes ${nodes} --nproc_per_node 8 --master_addr ${leader} --master_port ${job_port} /main.py
