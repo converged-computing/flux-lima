77d9c01128cedd0c49cef1153fd33e088c9d2fcd
vsoch <vsoch@users.noreply.github.com>
2023-12-29 08:02:01 -0700

    working container phew
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/mnist/launch-container.sh
+++ usernetes/scripts/mnist/launch-container.sh
@@ -8,8 +8,15 @@ nodes=6
 # This is the job name, index, service name, and namespace (default)
 leader="flux-sample-0.flux-service.default.svc.cluster.local"
 
+# Cheat to get the rank - last bit of hostname
+# The job index variable doesn't seem to work here, I think it needs to be exported
+name=$(hostname)
+IFS='-' read -ra ADDR <<< "${name}"
+for i in "${ADDR[@]}"; do
+  rank=${i}
+done
+
 echo "The leader broker is ${leader}"
-rank=${JOB_COMPLETION_INDEX}
 echo "I am hostname $(hostname) and rank ${rank} of ${nodes} nodes"
 
 time torchrun --node_rank ${rank} --nnodes ${nodes} --nproc_per_node 8 --master_addr ${leader} --master_port ${job_port} /main.py
