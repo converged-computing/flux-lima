af51fab5121c6d2e89d6424a6b61c0f4b5a9f0b6
vsoch <vsoch@users.noreply.github.com>
2023-12-29 17:41:18 -0700

    add mnist single node
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ usernetes/scripts/mnist/mnist-single-node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+job_name="flux-sample"
+job_port="8080"
+container="/home/flux/mnist/pytorch-mnist-cpu_latest.sif"
+
+# Hard code the leader for now
+leader=$(hostname)
+nodes=${FLUX_JOB_NNODES}
+rank=${FLUX_TASK_RANK}
+echo "I am hostname $(hostname) and rank ${rank} of ${nodes} nodes"
+time singularity exec ${container} torchrun --node_rank ${rank} --nnodes ${nodes} --nproc_per_node 8 --master_addr ${leader} --master_port ${job_port} /home/flux/mnist/main.py
