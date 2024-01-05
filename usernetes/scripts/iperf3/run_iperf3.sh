db7fbe7565f109eb5fce2a7cf530e74dc19c142a
vsoch <vsoch@users.noreply.github.com>
2024-01-04 19:36:04 -0700

    logfile option does not seem to work
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/iperf3/run_iperf3.sh
+++ usernetes/scripts/iperf3/run_iperf3.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 server=${1}
 client=${2}
-logfile=${3}
 echo "=== SERVER: ${server}"
 echo "CLIENT: ${client}"
-iperf3 -c ${server} -p 8080 --json > ${logfile}.json
+iperf3 -c ${server} -p 8080 --json
 echo "CLIENT BIDIRECTIONAL: ${client}"
 # Will run 20x, bidirectional
-iperf3 -c ${server} -p 8080 --json -bidir > ${logfile}-bidir.json
+iperf3 -c ${server} -p 8080 --json -bidir
