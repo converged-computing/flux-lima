1a81e99032f5bb65b501a57341a4c67b01703311
vsoch <vsoch@users.noreply.github.com>
2024-01-04 19:10:00 -0700

    add client for verbosity
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/scripts/iperf3/run_iperf3.sh
+++ usernetes/scripts/iperf3/run_iperf3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 server=${1}
+client=${2}
 echo "=== SERVER: ${server}"
-echo "CLIENT: $(hostname)"
+echo "CLIENT: ${client}"
 iperf3 -c ${server} -p 8080
-echo "CLIENT BIDIRECTIONAL: $(hostname)"
+echo "CLIENT BIDIRECTIONAL: ${client}"
 # Will run 20x, bidirectional
 iperf3 -c ${server} -p 8080 -bidir
